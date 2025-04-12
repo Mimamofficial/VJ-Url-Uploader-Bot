827c5d9ff85d4df61c6d8808b0c6305670152844
Mimamofficial <mrmuzaffarimam@gmail.com>
2025-04-12 12:03:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ class Config(object):
     TECH_VJ_PROCESS_MAX_TIMEOUT = 3600
     
     # your telegram account id
-    TECH_VJ_OWNER_ID = int(os.environ.get("TECH_VJ_OWNER_ID", "6139759254 1038493510")) 
+    TECH_VJ_OWNER_ID = int(os.environ.get("TECH_VJ_OWNER_ID", "1038493510")) 
     TECH_VJ_SESSION_NAME = "VJ-URL-UPLOADER-BOT"
     
     # database uri (mongodb)
